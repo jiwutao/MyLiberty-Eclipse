4c01adef295258a9709f1933fdfdc74370ea15c3
Jim Tao <jiwutao@hotmail.com>
2017-02-28 14:23:16 -0500

    implements the tone analyzer call

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+
 /**
  * Servlet implementation class SimpleServlet
  */
@@ -21,7 +24,30 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        // response.getWriter().print("Hello World!");
+        String text =
+        	      "I know the times are difficult! Our sales have been "
+        	          + "disappointing for the past three quarters for our data analytics "
+        	          + "product suite. We have a competitive data analytics product "
+        	          + "suite in the industry. But we need to do our job selling it! "
+        	          + "We need to acknowledge and fix our sales challenges. "
+        	          + "We can’t blame the economy for our lack of execution! "
+        	          + "We are missing critical sales opportunities. "
+        	          + "Our product is in no way inferior to the competitor products. "
+        	          + "Our clients are hungry for analytical tools to improve their "
+        	          + "business outcomes. Economy has nothing to do with it.";
+        response.getWriter().print(AnalyzeTone(text));
     }
 
+    
+    private String AnalyzeTone(String text) {
+    ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
+    service.setUsernameAndPassword("b5cd853e-e974-416b-9448-0d6d4697a882", "pXYljtxiZqlT");
+
+    // Call the service and get the tone
+    ToneAnalysis tone = service.getTone(text, null).execute();
+    System.out.println(tone);
+    return (tone.toString());
+    }
+    
 }
