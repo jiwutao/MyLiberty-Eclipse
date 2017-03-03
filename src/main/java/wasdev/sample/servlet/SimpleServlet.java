93f1b7d37e3bc8c31a9c767a287215d76c0f4772
Jim Tao <jiwutao@hotmail.com>
2017-03-03 10:58:07 -0500

    Text changed.

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -26,16 +26,17 @@ public class SimpleServlet extends HttpServlet {
         response.setContentType("application/json");
         // response.getWriter().print("Hello World!");
         String text =
-        	      "I know the times are difficult! Our sales have been "
-        	          + "disappointing for the past three quarters for our data analytics "
-        	          + "product suite. We have a competitive data analytics product "
-        	          + "suite in the industry. But we need to do our job selling it! "
-        	          + "We need to acknowledge and fix our sales challenges. "
-        	          + "We can’t blame the economy for our lack of execution! "
-        	          + "We are missing critical sales opportunities. "
-        	          + "Our product is in no way inferior to the competitor products. "
-        	          + "Our clients are hungry for analytical tools to improve their "
-        	          + "business outcomes. Economy has nothing to do with it.";
+//        	      "I know the times are difficult! Our sales have been "
+//        	          + "disappointing for the past three quarters for our data analytics "
+//        	          + "product suite. We have a competitive data analytics product "
+//        	          + "suite in the industry. But we need to do our job selling it! "
+//        	          + "We need to acknowledge and fix our sales challenges. "
+//        	          + "We can’t blame the economy for our lack of execution! "
+//        	          + "We are missing critical sales opportunities. "
+//        	          + "Our product is in no way inferior to the competitor products. "
+//        	          + "Our clients are hungry for analytical tools to improve their "
+//        	          + "business outcomes. Economy has nothing to do with it.";
+        		"I saw a worm in my pizza.";
         response.getWriter().print(AnalyzeTone(text));
     }
 
